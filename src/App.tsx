2c625e543ed5f4bf97294ce9932a1bc7e92cd051
Maximilian <scientificresearch309@gmail.com>
2025-10-01 00:58:28 +0200

    NavLink and Routes are working without CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 // import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { PageWelcome } from './components/PageWelcome';
+import { PageInfo } from './components/PageInfo';
+import { PageAbout } from './components/PageAbout';
 
 function App() {
   return (
     <div className="App">
       <h1>React-Vite-Typescript-FRONTEND</h1>
+      <nav>
+        <NavLink to="welcome">Welcome</NavLink>
+        <NavLink to="info">Info</NavLink>
+        <NavLink to="about">About</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="welcome" element={<PageWelcome />}></Route>
+        <Route path="info" element={<PageInfo />}></Route>
+        <Route path="about" element={<PageAbout />}></Route>
+      </Routes>
     </div>
   );
 }
