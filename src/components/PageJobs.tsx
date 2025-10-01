229f639eb3a6ab00e8e3f437c0fe29a721dbcf86
Maximilian <scientificresearch309@gmail.com>
2025-10-01 20:30:21 +0200

    getting the jobs using map without css

--- src/components/PageJobs.tsx
+++ src/components/PageJobs.tsx
@@ -1,7 +1,33 @@
+import React from 'react';
+import Jobs from '../data/jobs.json';
+
+interface IJobs {
+  id: number;
+  title: string;
+  company: string;
+  url: string;
+  description: string;
+  skillList: string;
+  todo: string;
+}
+
 export const PageJobs = () => {
-        return (
-            <>
-                <p>Welcome to the Jobs page.</p>
-            </>
-        );
-    };
+  return (
+    <div className="pageJobs">
+      <h1>There are {Jobs.length} jobs:</h1>
+      <div className="jobs">
+        {Jobs.map((job) => (
+          <div className="job">
+            <React.Fragment key={job.id}>
+              <h2>{job.title}</h2>
+              <p>{job.company}</p>
+              <p>{job.description}</p>
+              <p>{job.skillList}</p>
+              <p>{job.todo}</p>
+            </React.Fragment>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
