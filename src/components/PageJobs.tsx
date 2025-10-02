86490adfe8e6f141f286f734be8674d2a30d196c
Maximilian <scientificresearch309@gmail.com>
2025-10-02 12:16:26 +0200

    axios with url

--- src/components/PageJobs.tsx
+++ src/components/PageJobs.tsx
@@ -22,13 +22,17 @@ const jobs_defaultValues = {
   todo: 'NONE',
 };
 
+// const url = 'http://localhost:8000/jobs';
+const url = 'http://localhost:8000';
+
 export const PageJobs = () => {
   const [jobs, setJobs] = useState<IJobs[]>([]); // without default value
   // const [jobs, setJobs] = useState([jobs_defaultValues]); // or with default value
 
   useEffect(() => {
     (async () => {
-      const jobs_API = (await axios.get('http://localhost:8000/jobs')).data;
+      // const jobs_API = (await axios.get('http://localhost:8000/jobs')).data; OR with url
+      const jobs_API = (await axios.get(`${url}/jobs`)).data;
       console.log(jobs_API);
       const _jobs = [...jobs_API];
       setJobs(_jobs);
